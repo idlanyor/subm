b7840aa5bbb04add57ad31c0ee926822c918c756
roynaldi3301 <prasbhara0604@gmail.com>
2023-08-09 17:37:52 +0700

    remove fontawesome

--- assets/js/luring.js
+++ assets/js/luring.js
@@ -1,86 +1,65 @@
-// matkul.js
+// luring.js
 const tBody = document.querySelector('tBody');
 const tHead = document.querySelector('thead');
-const dataMatkul = [
-    {
-        kodeMatkul: '#33',
-        mataKuliah: 'Struktur Data',
-        dosenPengampu: 'Rahmam Manto, S.Kom',
-        hari: 'Selasa',
-        jam: '19.30 - 20.30',
-        prodi: 'STE & SSI',
-    },
-    {
-        kodeMatkul: '#44',
-        mataKuliah: 'Basis Data',
-        dosenPengampu: 'Siti Delimasari',
-        hari: 'Rabu',
-        jam: '19.00 - 20.00',
-        prodi: 'DTI & SSI',
-    }, {
-        kodeMatkul: '#95',
-        mataKuliah: 'English 2',
-        dosenPengampu: 'Atika Shinta N,S.Pd',
-        hari: 'Senin',
-        jam: '19.00 - 19.45',
-        prodi: 'SSI,DKA,DTI.STE',
-    }, {
-        kodeMatkul: '#82',
-        mataKuliah: 'Interaksi Manusia & Komputer',
-        dosenPengampu: 'Febri Meliana K.R.,S.Kom',
-        hari: 'Jumat',
-        jam: '17.30 - 18.30',
-        prodi: 'SSI',
-    },
-]
 
-export async function showMatkul() {
+async function readDataFromFile() {
+    const response = await fetch('./data/dataLuring.json');
+    const dataLuring = await response.json();
+    return dataLuring;
+}
+
+export async function showLuring() {
     try {
+        const dataLuring = await readDataFromFile();
         let tr = '';
-        // Tampilkan data mata kuliah ke dalam tabel
-        // ...
-        tBody.innerHTML = tr;
 
-        // Perbarui <thead> sesuai dengan data <tbody> yang ditampilkan
-        const headRow = `
-            <tr class="text-center">
-                <th>No</th>
-                <th>Kode</th>
-                <th>Nama Mata Kuliah</th>
-                <th>Dosen</th>
-                <th>Hari</th>
-                <th>Jam</th>
-                <th>Prodi</th>
-                <th>Action</th>
-            </tr>
-        `;
-        
+        if (dataLuring.length === 0) {
+            tr = '<tr><td colspan="8" class="text-center">Tidak ada data Mata Kuliah yang tersedia</td></tr>';
+        } else {
+            // Tampilkan data mata kuliah ke dalam tabel
+            // ...
 
-        dataMatkul.forEach((matkul, index) => {
-            const row = `
-                    <tr>
-                        <td>${index + 1}</td>
-                        <td>${matkul.kodeMatkul}</td>
-                        <td>${matkul.mataKuliah}</td>
-                        <td>${matkul.dosenPengampu}</td>
-                        <td>${matkul.hari}</td>
-                        <td>${matkul.jam}</td>
-                        <td>${matkul.prodi}</td>
-                        <td>
-                            <button class="btn btn-primary btn-sm "><span class="material-icons md-18">visibility</span></button>
-                            <button class="btn btn-success btn-sm "><span class="material-icons md-18">edit</span></button>
-                            <button class="btn btn-danger btn-sm "><span class="material-icons md-18">delete</span></button>
-                        </td>
-                    </tr>
-                    `;
-            tr += row;
-        });
-        tHead.innerHTML = headRow;
+            // Perbarui <thead> sesuai dengan data <tbody> yang ditampilkan
+            const headRow = `
+                <tr class="text-center">
+                    <th>No</th>
+                    <th>Kode</th>
+                    <th>Nama Mata Kuliah</th>
+                    <th>Dosen</th>
+                    <th>Hari</th>
+                    <th>Jam</th>
+                    <th>Prodi</th>
+                    <th>Action</th>
+                </tr>
+            `;
+
+            dataLuring.forEach((luring, index) => {
+                const row = `
+                        <tr>
+                            <td>${index + 1}</td>
+                            <td>#${luring.kodeLuring}</td>
+                            <td>${luring.mataKuliah}</td>
+                            <td>${luring.dosenPengampu}</td>
+                            <td>${luring.hari}</td>
+                            <td>${luring.jam}</td>
+                            <td>${luring.prodi}</td>
+                            <td>
+                                <button class="btn btn-primary btn-sm "><span class="material-icons md-18">visibility</span></button>
+                                <button class="btn btn-success btn-sm "><span class="material-icons md-18">edit</span></button>
+                                <button class="btn btn-danger btn-sm "><span class="material-icons md-18">delete</span></button>
+                            </td>
+                        </tr>
+                        `;
+                tr += row;
+            });
+
+            tHead.innerHTML = headRow;
+        }
+
         tBody.innerHTML = tr;
-        return dataMatkul.length;
+        return dataLuring.length;
     } catch (error) {
         console.error('Terjadi kesalahan:', error);
         return 0;
     }
 }
-// ...
--- assets/js/prodi.js
+++ assets/js/prodi.js
@@ -1,81 +1,61 @@
 // matkul.js
 const tBody = document.querySelector('tBody');
 const tHead = document.querySelector('thead');
-const dataMatkul = [
-    {
-        kodeMatkul: '#33',
-        mataKuliah: 'Struktur Data',
-        dosenPengampu: 'Rahmam Manto, S.Kom',
-        hari: 'Selasa',
-        jam: '19.30 - 20.30',
-        prodi: 'STE & SSI',
-    },
-    {
-        kodeMatkul: '#44',
-        mataKuliah: 'Basis Data',
-        dosenPengampu: 'Siti Delimasari',
-        hari: 'Rabu',
-        jam: '19.00 - 20.00',
-        prodi: 'DTI & SSI',
-    }, {
-        kodeMatkul: '#95',
-        mataKuliah: 'English 2',
-        dosenPengampu: 'Atika Shinta N,S.Pd',
-        hari: 'Senin',
-        jam: '19.00 - 19.45',
-        prodi: 'SSI,DKA,DTI.STE',
-    }, {
-        kodeMatkul: '#82',
-        mataKuliah: 'Interaksi Manusia & Komputer',
-        dosenPengampu: 'Febri Meliana K.R.,S.Kom',
-        hari: 'Jumat',
-        jam: '17.30 - 18.30',
-        prodi: 'SSI',
-    },
-]
+
+async function readDataFromFile() {
+    const response = await fetch('./data/dataMatkul.json');
+    const dataMatkul = await response.json();
+    return dataMatkul;
+}
 
 export async function showMatkul() {
     try {
+        const dataMatkul = await readDataFromFile();
         let tr = '';
-        // Tampilkan data mata kuliah ke dalam tabel
-        // ...
-        tBody.innerHTML = tr;
 
-        // Perbarui <thead> sesuai dengan data <tbody> yang ditampilkan
-        const headRow = `
-            <tr class="text-center">
-                <th>No</th>
-                <th>Kode</th>
-                <th>Nama Mata Kuliah</th>
-                <th>Dosen</th>
-                <th>Hari</th>
-                <th>Jam</th>
-                <th>Prodi</th>
-                <th>Action</th>
-            </tr>
-        `;
-        
+        if (dataMatkul.length === 0) {
+            tr = '<tr><td colspan="8" class="text-center">Tidak ada data Mata Kuliah yang tersedia</td></tr>';
+        } else {
+            // Tampilkan data mata kuliah ke dalam tabel
+            // ...
 
-        dataMatkul.forEach((matkul, index) => {
-            const row = `
-                    <tr>
-                        <td>${index + 1}</td>
-                        <td>${matkul.kodeMatkul}</td>
-                        <td>${matkul.mataKuliah}</td>
-                        <td>${matkul.dosenPengampu}</td>
-                        <td>${matkul.hari}</td>
-                        <td>${matkul.jam}</td>
-                        <td>${matkul.prodi}</td>
-                        <td>
-                            <button class="btn btn-primary btn-sm "><span class="material-icons md-18">visibility</span></button>
-                            <button class="btn btn-success btn-sm "><span class="material-icons md-18">edit</span></button>
-                            <button class="btn btn-danger btn-sm "><span class="material-icons md-18">delete</span></button>
-                        </td>
-                    </tr>
-                    `;
-            tr += row;
-        });
-        tHead.innerHTML = headRow;
+            // Perbarui <thead> sesuai dengan data <tbody> yang ditampilkan
+            const headRow = `
+                <tr class="text-center">
+                    <th>No</th>
+                    <th>Kode</th>
+                    <th>Nama Mata Kuliah</th>
+                    <th>Dosen</th>
+                    <th>Hari</th>
+                    <th>Jam</th>
+                    <th>Prodi</th>
+                    <th>Action</th>
+                </tr>
+            `;
+
+            dataMatkul.forEach((matkul, index) => {
+                const row = `
+                        <tr>
+                            <td>${index + 1}</td>
+                            <td>#${matkul.kodeMatkul}</td>
+                            <td>${matkul.mataKuliah}</td>
+                            <td>${matkul.dosenPengampu}</td>
+                            <td>${matkul.hari}</td>
+                            <td>${matkul.jam}</td>
+                            <td>${matkul.prodi}</td>
+                            <td>
+                                <button class="btn btn-primary btn-sm "><span class="material-icons md-18">visibility</span></button>
+                                <button class="btn btn-success btn-sm "><span class="material-icons md-18">edit</span></button>
+                                <button class="btn btn-danger btn-sm "><span class="material-icons md-18">delete</span></button>
+                            </td>
+                        </tr>
+                        `;
+                tr += row;
+            });
+
+            tHead.innerHTML = headRow;
+        }
+
         tBody.innerHTML = tr;
         return dataMatkul.length;
     } catch (error) {
@@ -83,4 +63,3 @@ export async function showMatkul() {
         return 0;
     }
 }
-// ...
